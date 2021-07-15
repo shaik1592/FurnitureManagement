cec26428875ddaadcf48171a58635b350bafe716
shaik1592 <87200045+shaik1592@users.noreply.github.com>
2021-07-15 22:57:25 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿create table Furniture
+(ID int primary key identity,
+FurnitureName varchar(200),
+FurnitureCategory varchar(250),
+Price float)
+
+select * from Furniture
+
+insert into Furniture values('SleepWell', 'Beds', 2300)
+insert into Furniture values('TeakWood', 'Dining', 8300)
